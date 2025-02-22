12a7f3f4d5dea484f49b5f00c9a50f88b4c28985
Spenser <18bf4e7@proton.me>
2025-02-22 16:35:07 -0500

    Initial Commit

---
+++ WeSplitUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplitUI
+//
+//  Created by Spenser Dubin on 2/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplitUI/WeSplitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitUIApp.swift
+//  WeSplitUI
+//
+//  Created by Spenser Dubin on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
